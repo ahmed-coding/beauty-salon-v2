f93b972670c4cf6b4b743d50ea72a8ca845d9cdf
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2023-12-02 07:49:35 +0300

    update source

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -6,7 +6,7 @@ app_icon = "fa fa-th"
 app_color = "#e74c3c"
 app_email = "info@erpnext.com"
 app_license = "GNU General Public License (v3)"
-source_link = "https://github.com/frappe/erpnext"
+source_link = "https://github.com/ahmed-coding/beauty-salon-v2"
 app_logo_url = "/assets/erpnext/images/erpnext-logo.svg"
 
 
